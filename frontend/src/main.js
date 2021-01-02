060e52ce24cae11e7399ae34692293a185a15698
Juglipaff <juglipaff@gmail.com>
2021-01-02 14:07:57 +0300

    add login page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTelegramPlane } from '@fortawesome/free-brands-svg-icons'
 
+library.add(faTelegramPlane)
+// library.add(FontAwesomeBrandsIcon)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
