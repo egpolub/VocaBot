34df35abfe3a50a42880db9d1bc935c0d426afab
egpolub <egpolub@gmail.com>
2021-03-22 02:15:29 +0300

    fix typo

--- backend/src/main/java/ru/jpol/vocabot/VocabotApplication.java
+++ backend/src/main/java/ru/jpol/vocabot/VocabotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VocaBotApplication {
+public class VocabotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VocaBotApplication.class, args);
+        SpringApplication.run(VocabotApplication.class, args);
     }
 
 }
